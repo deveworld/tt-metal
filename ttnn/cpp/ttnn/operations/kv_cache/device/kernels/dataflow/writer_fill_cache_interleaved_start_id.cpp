36be4b1431433ae010f5fd5c898a245a5dfe990b
World <world@worldsw.dev>
2026-04-03 12:44:52 +0900

    Port batched fill_cache kernel to v0.65 dataflow API

--- ttnn/cpp/ttnn/operations/kv_cache/device/kernels/dataflow/writer_fill_cache_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/kv_cache/device/kernels/dataflow/writer_fill_cache_interleaved_start_id.cpp
@@ -2,10 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "api/dataflow/dataflow_api.h"
-#include "experimental/noc.h"
-#include "experimental/circular_buffer.h"
-#include "experimental/tensor.h"
+#include "dataflow_api.h"
 
 void kernel_main() {
     const uint32_t dst_addr = get_arg_val<uint32_t>(0);
@@ -24,8 +21,6 @@ void kernel_main() {
     const uint32_t rows_per_batch = num_heads * input_Ht;
     const uint32_t page_bytes = get_local_cb_interface(cb_id_out).fifo_page_size;
 
-    experimental::Noc noc;
-    experimental::CircularBuffer cb(cb_id_out);
     const auto s = TensorAccessor(dst_args, dst_addr, page_bytes);
 
     for (uint32_t row_id = start_row_num; row_id < start_row_num + num_rows; ++row_id) {
@@ -36,13 +31,15 @@ void kernel_main() {
         uint32_t cache_tile_id =
             batch_start_id + batch_idx * cache_CHtWt + head_idx * cache_HtWt + seq_idx * Wt;
 
+        cb_wait_front(cb_id_out, Wt);
+        uint32_t out_l1_read_addr = get_read_ptr(cb_id_out);
         for (uint32_t tile = 0; tile < Wt; ++tile) {
-            cb.wait_front(1);
-            noc.async_write(cb, s, page_bytes, {}, {.page_id = cache_tile_id});
-            noc.async_writes_flushed();
-            cb.pop_front(1);
+            noc_async_write_tile(cache_tile_id, s, out_l1_read_addr);
+            out_l1_read_addr += page_bytes;
             cache_tile_id += 1;
         }
+        noc_async_writes_flushed();
+        cb_pop_front(cb_id_out, Wt);
     }
-    noc.async_write_barrier();
+    noc_async_write_barrier();
 }
